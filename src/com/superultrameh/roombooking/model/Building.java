68d557c11b765aa7aa276fdea233c20cad323d02
beckygowland <beckygowland@gmail.com>
2014-03-31 22:51:23 -0400

    added model folder

---
+++ src/com/superultrameh/roombooking/model/Building.java
@@ -0,0 +1,29 @@
+package com.superultrameh.roombooking.model;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.superultrameh.roombooking.R;
+import com.superultrameh.roombooking.fragments.AlertsFragment;
+import com.superultrameh.roombooking.fragments.ApproveTradesFragment;
+import com.superultrameh.roombooking.fragments.MessageStaffFragment;
+import com.superultrameh.roombooking.fragments.SettingsFragment;
+import com.superultrameh.roombooking.fragments.StaffFragment;
+import com.superultrameh.roombooking.fragments.TradeZoneFragment;
+
+public class Building {
+
+}
